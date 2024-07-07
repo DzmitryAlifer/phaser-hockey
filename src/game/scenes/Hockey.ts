b0e65bd9cf07d43b6d751e162edb420072bd4533
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2024-07-07 23:30:08 +0200

    1

--- src/game/scenes/Hockey.ts
+++ src/game/scenes/Hockey.ts
@@ -11,7 +11,7 @@ export class Hockey extends Scene {
         const field = this.add.graphics();
         field.fillStyle(0xdddddd, 1).fillRoundedRect(0, 0, FIELD_SIZE_X, FIELD_SIZE_Y, FIELD_CORNER_RADIUS);
         this.fieldContainer = this.add.container(0, 0, field);
-        this.fieldContainer.setSize(FIELD_SIZE_X, FIELD_SIZE_Y);
+        this.fieldContainer.setSize(FIELD_SIZE_X + 200, FIELD_SIZE_Y + 200);
 
         const puck = this.add.circle(0, 0, PUCK_SIZE / 2, 0x333333);
         this.puckContainer = this.add.container(FIELD_SIZE_X / 2, FIELD_SIZE_Y / 2, puck);
