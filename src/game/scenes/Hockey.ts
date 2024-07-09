0c50698ad8ece2e4ef08f9f386cf948d4f0f8b3d
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2024-07-09 23:06:20 +0200

    1

--- src/game/scenes/Hockey.ts
+++ src/game/scenes/Hockey.ts
@@ -1,5 +1,5 @@
 import { Game, Geom, Scene } from 'phaser';
-import { FIELD_CORNER_RADIUS, SIZE_X, SIZE_Y, PUCK_SIZE } from '../constants';
+import { CORNER, SIZE_X, SIZE_Y, PUCK_SIZE } from '../constants';
 
 export class Hockey extends Scene {
     fieldContainer!: any;
@@ -8,16 +8,28 @@ export class Hockey extends Scene {
     outerRect!: any;
     innerRect!: any;
 
+    preload() {
+        this.load.image('ball', 'https://labs.phaser.io/assets/sprites/pangball.png');
+    }
+
     create() {
         this.cameras.main.centerOn(0, 0);
         this.matter.world.setBounds(-SIZE_X / 2, -SIZE_Y / 2);
 
-        const hockeyBoxOuterVerts = `${SIZE_X / 2} 0 ${SIZE_X} 0 ${SIZE_X} ${SIZE_Y} 0 ${SIZE_Y} 0 0 ${SIZE_X / 2 - 1} 0`;
-        const hockeyBoxInnerVerts = `${SIZE_X / 2 - 1} 10 10 10 10 ${SIZE_Y - 10} ${SIZE_X - 10} ${SIZE_Y - 10} ${SIZE_X - 10} 10 ${SIZE_X / 2} 10`;
-        const hockeyBoxVerts = `${hockeyBoxOuterVerts} ${hockeyBoxInnerVerts}`;
-        const hockeyBoxPolygon = this.add.polygon(0, 0, hockeyBoxVerts, 0x00ff00, 0.2);
-        this.matter.add.gameObject(hockeyBoxPolygon, { shape: { type: 'fromVerts', verts: hockeyBoxVerts, flagInternal: false } });
+        const boxOuterVerts = `${SIZE_X / 2} 0 ${SIZE_X} 0 ${SIZE_X} ${SIZE_Y} 0 ${SIZE_Y} 0 0 ${SIZE_X / 2 - 1} 0`;
+        const boxInnerVerts = `${SIZE_X / 2 - 1} 10 ${CORNER + 10} 10 10 ${CORNER + 10} 10 ${SIZE_Y - 10} ${SIZE_X - 10} ${SIZE_Y - 10} ${SIZE_X - 10} 10 ${SIZE_X / 2} 10`;
+        const boxVerts = `${boxOuterVerts} ${boxInnerVerts}`;
+        const boxPolygon = this.add.polygon(0, 0, boxVerts, 0x00ff00, 0.2);
+        this.matter.add.gameObject(boxPolygon, { shape: { type: 'fromVerts', verts: boxVerts, flagInternal: false } });
 
+        const puck = this.add.circle(0, 0, 10, 0x333333);
+        const puckBody = this.matter.add.circle(0, 0, 10, { restitution: 1, friction: 0, frictionStatic: 0, frictionAir: 0.01 });
+      
+        this.matter.add.gameObject(puck, puckBody);
+  
+        
+
+        this.matter.add.mouseSpring();
     }
 
 }
@@ -30,7 +42,10 @@ const config = {
     backgroundColor: '#aaa',
     physics: {
         default: 'matter',
-        matter: { debug: true },
+        matter: {
+            debug: true,
+            gravity: { x: 0, y: 0 },
+        },
     },
 };
 
