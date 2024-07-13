a91c6a8eaf08c59aa3fa6d869c35a9d03c0de0d9
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2024-07-13 20:35:41 +0200

    1

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -5,4 +5,4 @@ export const CORNER_R = 55;
 export const CORNER_D = CORNER_R * 2;
 export const BORDER_BLOCK_RADIUS = 16;
 export const RADIAL_BLOCK_SHIFT = CORNER_D - BORDER_BLOCK_RADIUS;
-export const BLOCK_AMOUNT = 8;
+export const BLOCK_AMOUNT = 64;
--- src/game/scenes/Hockey3.ts
+++ src/game/scenes/Hockey3.ts
@@ -18,28 +18,17 @@ export class Hockey3 extends Scene {
         const straightBorder2 = this.add.rectangle(0, SIZE_Y / 2, SIZE_X, 1);
         const straightBorder3 = this.add.rectangle(-SIZE_X / 2, 0, 1, SIZE_Y);
         const straightBorder4 = this.add.rectangle(SIZE_X / 2, 0, 1, SIZE_Y);
-        
         straightBorderGroup.add(straightBorder1).add(straightBorder2).add(straightBorder3).add(straightBorder4);
-
-        for (const straightBorder of straightBorderGroup.getChildren()) {
-            (straightBorder.body as any).setImmovable(true);
-        }
-
-        const puck = this.physics.add.image(-100, -50, '').setCircle(6).setVelocity(-200, -80).setBounce(1);
+        straightBorderGroup.getChildren().forEach(({ body }) => (body as any).setImmovable(true));
 
         const radialBorderGroup = this.physics.add.group({ defaultKey: 'radialBorder' });
         createRadialBorder(radialBorderGroup, RADIAL_BLOCK_SHIFT - SIZE_X / 2, RADIAL_BLOCK_SHIFT - SIZE_Y / 2, 180);
         createRadialBorder(radialBorderGroup, SIZE_X / 2 - RADIAL_BLOCK_SHIFT, RADIAL_BLOCK_SHIFT - SIZE_Y / 2, 270);
         createRadialBorder(radialBorderGroup, SIZE_X / 2 - RADIAL_BLOCK_SHIFT, SIZE_Y / 2 - RADIAL_BLOCK_SHIFT, 0);
         createRadialBorder(radialBorderGroup, RADIAL_BLOCK_SHIFT - SIZE_X / 2, SIZE_Y / 2 - RADIAL_BLOCK_SHIFT, 90);
-        
-        const radialBorderBlocks = radialBorderGroup.getChildren();
-        
-        for (let i = 0; i < radialBorderBlocks.length; i++) {
-            const borderBlock = radialBorderBlocks.at(i) as any;
-            borderBlock.angle = i * 90 / BLOCK_AMOUNT;
-            borderBlock.body.setImmovable(true);
-        }
+        radialBorderGroup.getChildren().forEach(({ body }) => (body as any).setCircle(16).setImmovable(true));
+
+        const puck = this.physics.add.image(-100, -50, '').setCircle(6).setVelocity(-200, -80).setBounce(1);
 
         this.physics.add.collider(puck, radialBorderGroup);
         this.physics.add.collider(puck, straightBorderGroup);
