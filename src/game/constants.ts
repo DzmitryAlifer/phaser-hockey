747a11e15c3cfaa493a63a5ad24742725f22a792
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2024-07-13 13:35:35 +0200

    1

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -3,3 +3,5 @@ export const SIZE_X = 800;
 export const SIZE_Y = 400;
 export const CORNER_R = 55;
 export const CORNER_D = CORNER_R * 2;
+export const BORDER_BLOCK_RADIUS = 16;
+export const RADIAL_BLOCK_SHIFT = CORNER_D - BORDER_BLOCK_RADIUS;
--- src/game/scenes/Hockey3.ts
+++ src/game/scenes/Hockey3.ts
@@ -1,5 +1,5 @@
-import { Curves, Game, Geom, Math, Scene } from 'phaser';
-import { SIZE_X, SIZE_Y, CORNER_D } from '../constants';
+import { Curves, Game, Geom, Math, Physics, Scene } from 'phaser';
+import { RADIAL_BLOCK_SHIFT, SIZE_X, SIZE_Y, CORNER_D } from '../constants';
 
 export class Hockey3 extends Scene {
     constructor() {
@@ -14,26 +14,29 @@ export class Hockey3 extends Scene {
 
     create() {
         this.cameras.main.centerOn(0, 0);
-        const puck = this.physics.add.image(-100, -80, '').setCircle(6).setVelocity(-200, -50).setBounce(0.99);
-        const borders = this.physics.add.group({ defaultKey: 'ball', bounceX: 1, bounceY: 1 });
+        this.matter.world.setBounds(-SIZE_X / 2, -SIZE_Y / 2);
 
-        const createdBalls = borders.createMultiple({ quantity: 60, key: borders.defaultKey, frame: 0 });
-        Phaser.Actions.PlaceOnCircle(createdBalls, { x: -200, y: 0, radius: 150 } as Geom.Circle, Math.DegToRad(180), Math.DegToRad(270));
+        const puck = this.physics.add.image(-100, -40, '').setCircle(6).setVelocity(-200, -50).setBounce(0.99);
+        const radialBorder = this.physics.add.group({ defaultKey: 'radialBorder' });
+        createRadialBorder(radialBorder, RADIAL_BLOCK_SHIFT - SIZE_X / 2, RADIAL_BLOCK_SHIFT - SIZE_Y / 2, 180);
+        createRadialBorder(radialBorder, SIZE_X / 2 - RADIAL_BLOCK_SHIFT, RADIAL_BLOCK_SHIFT - SIZE_Y / 2, 270);
+        createRadialBorder(radialBorder, SIZE_X / 2 - RADIAL_BLOCK_SHIFT, SIZE_Y / 2 - RADIAL_BLOCK_SHIFT, 0);
+        createRadialBorder(radialBorder, RADIAL_BLOCK_SHIFT - SIZE_X / 2, SIZE_Y / 2 - RADIAL_BLOCK_SHIFT, 90);
 
-        for (const border of borders.getChildren()) {
-            (border as any).setCircle(16);
-            (border.body as any).setImmovable(true);
-
+        for (const borderBlock of radialBorder.getChildren()) {
+            (borderBlock as any).setCircle(16);
+            (borderBlock.body as any).setImmovable(true);
         }
 
-        this.physics.add.collider(puck, borders);
+        this.physics.add.collider(puck, radialBorder);
     }
+
 }
 
 const config = {
     type: Phaser.AUTO,
-    width: SIZE_X + 20,
-    height: SIZE_Y + 20,
+    width: SIZE_X + 100,
+    height: SIZE_Y + 100,
     scene: Hockey3,
     physics: {
         default: 'matter',
@@ -43,5 +46,10 @@ const config = {
         }
     }
 };
+
+function createRadialBorder(group: Physics.Arcade.Group, x: number, y: number, startAngle: number): void {
+    const borderBlock = group.createMultiple({ quantity: 30, key: group.defaultKey, frame: 0 });
+    Phaser.Actions.PlaceOnCircle(borderBlock, { x, y, radius: CORNER_D } as Geom.Circle, Math.DegToRad(startAngle), Math.DegToRad(startAngle + 90));
+}
 
 export const startHockey3 = (parent: string) => new Game({ ...config, parent });
