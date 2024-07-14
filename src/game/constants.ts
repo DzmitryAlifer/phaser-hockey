386c2bdf26cb25d776f5f3a893e74bd1f1c510d6
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2024-07-14 11:15:43 +0200

    1

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -1,3 +1,5 @@
+import { Math } from 'phaser';
+
 export const PUCK_RADIUS = 4;
 export const PUCK_IMG_SIZE = 32;
 export const SIZE_Y = 400;
@@ -5,6 +7,17 @@ export const SIZE_X = SIZE_Y * 2;
 export const CORNER_R = 55;
 export const CORNER_D = CORNER_R * 2;
 export const CORNER_DRAW_R = 104;
+export const CIRCLE_RADIUS = SIZE_X / 13;
 export const BORDER_BLOCK_RADIUS = 16;
 export const RADIAL_BLOCK_SHIFT = CORNER_D - BORDER_BLOCK_RADIUS;
+export const NET_LINE_X_OFFSET = SIZE_X / 2 - SIZE_X / 15;
 export const BLOCK_AMOUNT = 64;
+
+export const DEGREE_90 = Math.DegToRad(90);
+export const DEGREE_180 = Math.DegToRad(180);
+export const DEGREE_270 = Math.DegToRad(270);
+export const DEGREE_360 = Math.DegToRad(360);
+
+export const ICE_RED = 0xdd0000;
+export const ICE_BLUE = 0x0000dd;
+export const ICE_ALPHA = 0.3;
--- src/game/scenes/Hockey.ts
+++ src/game/scenes/Hockey.ts
@@ -1,5 +1,5 @@
-import { Game, Geom, Math, Physics, Scene } from 'phaser';
-import { BLOCK_AMOUNT, CORNER_D, CORNER_DRAW_R, PUCK_IMG_SIZE, PUCK_RADIUS, RADIAL_BLOCK_SHIFT, SIZE_X, SIZE_Y } from '../constants';
+import { Game, GameObjects, Geom, Math, Physics, Scene } from 'phaser';
+import { BLOCK_AMOUNT, CIRCLE_RADIUS, CORNER_D, CORNER_DRAW_R, DEGREE_90, DEGREE_180, DEGREE_270, DEGREE_360, ICE_ALPHA, ICE_BLUE, ICE_RED, NET_LINE_X_OFFSET, PUCK_IMG_SIZE, PUCK_RADIUS, RADIAL_BLOCK_SHIFT, SIZE_X, SIZE_Y } from '../constants';
 
 export class Hockey3 extends Scene {
     constructor() {
@@ -32,25 +32,35 @@ export class Hockey3 extends Scene {
 
         this.add.image(-SIZE_X / 2, -SIZE_Y / 2, 'rink').setOrigin(0, 0);
 
-        const graphics = this.add.graphics();
-        graphics.lineStyle(4, 0xffff00, 1);
-        graphics.arc(CORNER_D - SIZE_X / 2 - 8, CORNER_D - SIZE_Y / 2 - 8, CORNER_DRAW_R, Math.DegToRad(180), Math.DegToRad(270));
-        graphics.arc(SIZE_X / 2 - CORNER_D + 8, CORNER_D - SIZE_Y / 2 - 8, CORNER_DRAW_R, Math.DegToRad(270), Math.DegToRad(360));
-        graphics.arc(SIZE_X / 2 - CORNER_D + 8, SIZE_Y / 2 - CORNER_D + 8, CORNER_DRAW_R, Math.DegToRad(0), Math.DegToRad(90));
-        graphics.arc(CORNER_D - SIZE_X / 2 - 8, SIZE_Y / 2 - CORNER_D + 8, CORNER_DRAW_R, Math.DegToRad(90), Math.DegToRad(180));
-        graphics.arc(CORNER_D - SIZE_X / 2 - 8, CORNER_D - SIZE_Y / 2 - 8, CORNER_DRAW_R, Math.DegToRad(180), Math.DegToRad(270));
-        graphics.strokePath();
+        const graphics = this.add.graphics({ fillStyle: { color: ICE_RED, alpha: ICE_ALPHA } });
+        graphics.lineStyle(4, ICE_RED, ICE_ALPHA)
+            // outter shape
+            .arc(CORNER_D - SIZE_X / 2 - 8, CORNER_D - SIZE_Y / 2 - 8, CORNER_DRAW_R, DEGREE_180, DEGREE_270)
+            .arc(SIZE_X / 2 - CORNER_D + 8, CORNER_D - SIZE_Y / 2 - 8, CORNER_DRAW_R, DEGREE_270, DEGREE_360)
+            .arc(SIZE_X / 2 - CORNER_D + 8, SIZE_Y / 2 - CORNER_D + 8, CORNER_DRAW_R, 0, DEGREE_90)
+            .arc(CORNER_D - SIZE_X / 2 - 8, SIZE_Y / 2 - CORNER_D + 8, CORNER_DRAW_R, DEGREE_90, DEGREE_180)
+            .arc(CORNER_D - SIZE_X / 2 - 8, CORNER_D - SIZE_Y / 2 - 8, CORNER_DRAW_R, DEGREE_180, DEGREE_270)
+            .strokePath()
+            // red lines
+            .lineBetween(0, -SIZE_Y / 2, 0, SIZE_Y / 2)
+            .lineBetween(-NET_LINE_X_OFFSET, -SIZE_Y / 2, -NET_LINE_X_OFFSET, SIZE_Y / 2)
+            .lineBetween(NET_LINE_X_OFFSET, -SIZE_Y / 2, NET_LINE_X_OFFSET, SIZE_Y / 2)
+            .beginPath().arc(-NET_LINE_X_OFFSET, 0, CIRCLE_RADIUS / 2, DEGREE_270, DEGREE_90).strokePath()
+
+        drawRedFaceOffCircle(graphics, -SIZE_X / 3.5, -SIZE_Y / 4);
+        drawRedFaceOffCircle(graphics, -SIZE_X / 3.5, SIZE_Y / 4);
+        drawRedFaceOffCircle(graphics, SIZE_X / 3.5, -SIZE_Y / 4);
+        drawRedFaceOffCircle(graphics, SIZE_X / 3.5, SIZE_Y / 4);
 
         const puck = this.physics.add.image(-100, -50, 'puck')
             .setScale(PUCK_RADIUS / PUCK_IMG_SIZE * 2)
             .setCircle(PUCK_IMG_SIZE / 2)
             .setVelocity(-200, -80)
-            .setBounce(.8);
+            .setBounce(0.8);
 
         this.physics.add.collider(puck, radialBorderGroup);
         this.physics.add.collider(puck, straightBorderGroup);
     }
-
 }
 
 const config = {
@@ -70,6 +80,17 @@ const config = {
 function createRadialBorder(group: Physics.Arcade.Group, x: number, y: number, startAngle: number): void {
     const borderBlock = group.createMultiple({ quantity: BLOCK_AMOUNT, key: group.defaultKey, frame: 0 });
     Phaser.Actions.PlaceOnCircle(borderBlock, { x, y, radius: CORNER_D } as Geom.Circle, Math.DegToRad(startAngle), Math.DegToRad(startAngle + 90));
+}
+
+function drawRedFaceOffCircle(graphics: GameObjects.Graphics, x: number, y: number): void {
+    graphics.beginPath()
+        .arc(x, y, CIRCLE_RADIUS, 0, DEGREE_360)
+        .strokePath()
+        .fillCircle(x, y, 6)
+        .lineBetween(x - 10, y - CIRCLE_RADIUS, x - 10, y - CIRCLE_RADIUS - 12)
+        .lineBetween(x + 10, y - CIRCLE_RADIUS, x + 10, y - CIRCLE_RADIUS - 12)
+        .lineBetween(x - 10, y + CIRCLE_RADIUS, x - 10, y + CIRCLE_RADIUS + 12)
+        .lineBetween(x + 10, y + CIRCLE_RADIUS, x + 10, y + CIRCLE_RADIUS + 12)
 }
 
 export const startHockey = (parent: string) => new Game({ ...config, parent });
